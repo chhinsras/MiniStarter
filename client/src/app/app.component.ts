0dc2c093b1e3efb29ccd85772371a5efb222df14
Chhin Sras <chhinsras@gmail.com>
2022-03-14 19:19:07 +0700

    Fixed translate start up

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { LocalStorageService } from './core/services/local-storage.service';
+import { MultilingualService } from './core/services/multilingual.service';
+import { ThemeService } from './core/services/theme.service';
+import { AccountService } from './core/services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  themeVariant: string = '';
+  darkModeIcon: string = '';
+  constructor(private accountService: AccountService, private translationService: MultilingualService, private themeService: ThemeService, private overlay: OverlayContainer) {
+  }
+
+  ngOnInit(): void {
+    this.loadCurrentUser();
+    this.loadDefaults();
+  }
+
+  loadDefaults() {
+    this.translationService.loadDefaultLanguage();
+    this.themeService.setThemeFromLocalStorage();
+  }
+
+  loadCurrentUser() {
+    this.accountService.loadCurrentUser()
+      .subscribe(() => {
+      }, error => {
+        console.log(error);
+      });
+  }
 }
