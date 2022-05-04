a42c43004662957bf879d2a3ad3fcc14806fc7f4
Chhin Sras <chhinsras@gmail.com>
2022-05-04 12:02:37 +0700

    Generate project run

--- hybrid/lib/main.dart
+++ hybrid/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Home Page'),
     );
   }
 }
