bce57b88c452f2469b7110ba25e08b67c5878130
Chhin Sras <chhinsras@gmail.com>
2022-05-04 19:43:10 +0700

    Flutter: cleanup main.dart

--- hybrid/lib/main.dart
+++ hybrid/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hybrid/providers/profile_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:hybrid/screens/screens.dart';
 import 'config/config.dart';
 
 void main() {
@@ -14,6 +14,7 @@ class MyApp extends ConsumerWidget {
   final profileProvider = ChangeNotifierProvider<ProfileProvider>((ref) {
     return ProfileProvider();
   });
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ProfileProvider profileManager = ref.watch(profileProvider);
@@ -26,54 +27,7 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       title: 'Mini Starter',
       theme: theme,
-      home: const MyHomePage(title: 'Mini Starter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomeScreen(title: 'Mini Starter'),
     );
   }
 }
---
+++ hybrid/lib/screens/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text(
+            'Home Page',
+          ),
+        ),
+      ),
+    );
+  }
+}
