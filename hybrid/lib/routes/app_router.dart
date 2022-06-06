1ec32c2634d7a1df097caf695ed3cb79fc162ab4
Chhin Sras <chhinsras@gmail.com>
2022-06-06 12:47:29 +0700

    removed some warnings

--- hybrid/lib/routes/app_router.dart
+++ hybrid/lib/routes/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'auth_guard.dart';
-import '../layouts/admin-layout.dart';
+import '../layouts/admin_layout.dart';
 import '../views/views.dart';
 
 part 'app_router.gr.dart';
