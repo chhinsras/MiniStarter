a3e91d3b1144295a2c7c136306fe12f48340c4f8
Chhin Sras <chhinsras@gmail.com>
2022-06-13 10:49:51 +0700

    added equatable to basemodel

--- hybrid/lib/models/base_model.dart
+++ hybrid/lib/models/base_model.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:equatable/equatable.dart';
 import 'package:hybrid/models/app_cache.dart';
 
-abstract class BaseModel extends ChangeNotifier {
+abstract class BaseModel extends ChangeNotifier with EquatableMixin {
   final ref = ProviderContainer();
   final appCache = AppCache();
 }
