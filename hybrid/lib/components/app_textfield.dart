926e3fe6acde40c46771b310c02cfcfdf18f8ccc
Chhin Sras <chhinsras@gmail.com>
2022-06-24 12:32:23 +0700

    update login page UI

--- hybrid/lib/components/app_textfield.dart
+++ hybrid/lib/components/app_textfield.dart
@@ -1,16 +1,19 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 class AppTextField extends StatelessWidget {
   AppTextField(
       {Key? key,
-      required this.hintText,
+      required this.label,
+      required this.placeholder,
       required this.formControlName,
       this.validationMessages,
       this.obscureText})
       : super(key: key);
-  final String hintText;
+  final String label;
+  final String placeholder;
   final String formControlName;
   Map<String, String> Function(FormControl<Object?>)? validationMessages;
   bool? obscureText = false;
@@ -21,24 +24,37 @@ class AppTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ReactiveTextField(
-      formControlName: formControlName,
-      validationMessages: validationMessages,
-      cursorColor: rwColor,
-      obscureText: obscureText ?? false,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.green,
-            width: 1.0,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: GoogleFonts.battambang(fontSize: 16),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        ReactiveTextField(
+          formControlName: formControlName,
+          validationMessages: validationMessages,
+          cursorColor: rwColor,
+          obscureText: obscureText ?? false,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.green,
+                width: 1.0,
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.green),
+            ),
+            hintText: placeholder,
+            hintStyle: const TextStyle(height: 0.5),
           ),
         ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.green),
-        ),
-        hintText: hintText,
-        hintStyle: const TextStyle(height: 0.5),
-      ),
+      ],
     );
   }
 }
--- hybrid/lib/views/account/login_page.dart
+++ hybrid/lib/views/account/login_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hybrid/components/app_submit_button.dart';
 import 'package:hybrid/components/app_textfield.dart';
+import 'package:hybrid/extensions/extensions.dart';
 import 'package:hybrid/helpers/helpers.dart';
 import 'package:hybrid/models/models.dart';
 import 'package:reactive_forms/reactive_forms.dart';
@@ -28,55 +31,130 @@ class LoginPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
           child: ReactiveForm(
             formGroup: loginForm,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 200,
-                  child: Image.asset(Assets.images.logo.path),
+            child: Card(
+              elevation: 8.0,
+              child: Container(
+                width: 1000,
+                height: 500,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
                 ),
-                const SizedBox(height: 16),
-                AppTextField(
-                  hintText: 'UserName',
-                  formControlName: 'userName',
-                  validationMessages: (control) => {
-                    ValidationMessage.required: 'The username must not be empty'
-                  },
-                ),
-                const SizedBox(height: 16),
-                AppTextField(
-                  hintText: 'Password',
-                  formControlName: 'password',
-                  validationMessages: (control) => {
-                    ValidationMessage.required: 'The password must not be empty'
-                  },
-                  obscureText: true,
-                ),
-                const SizedBox(height: 16),
-                AppSubmitButton(
-                  label: 'Submit',
-                  onPress: () => ref.watch(accountModel).login(loginForm.value),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Row(
                   children: [
-                    MaterialButton(
-                      onPressed: fillAdministration,
-                      child: const Text('Fill Administration'),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                context.localization
+                                    .translate('login_form.title'),
+                                style: GoogleFonts.moul(
+                                  fontSize: 20,
+                                )),
+                            Column(
+                              children: [
+                                AppTextField(
+                                  label: context.localization
+                                      .translate('login_form.username'),
+                                  placeholder: context.localization.translate(
+                                      'login_form.username_placeholder'),
+                                  formControlName: 'userName',
+                                  validationMessages: (control) => {
+                                    ValidationMessage.required:
+                                        context.localization.translate(
+                                            'login_form.username_required'),
+                                  },
+                                ),
+                                const SizedBox(height: 16),
+                                AppTextField(
+                                  label: context.localization
+                                      .translate('login_form.password'),
+                                  placeholder: context.localization.translate(
+                                      'login_form.password_placeholder'),
+                                  formControlName: 'password',
+                                  validationMessages: (control) => {
+                                    ValidationMessage.required:
+                                        context.localization.translate(
+                                            'login_form.password_required'),
+                                  },
+                                  obscureText: true,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Expanded(
+                                      child: AppSubmitButton(
+                                        label: context.localization.translate(
+                                            'login_form.login_button'),
+                                        onPress: () => ref
+                                            .watch(accountModel)
+                                            .login(loginForm.value),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Center(
+                                  child: TextButton(
+                                      onPressed: () {},
+                                      child: Text(
+                                        context.localization.translate(
+                                            'login_form.forgot_password'),
+                                        style: GoogleFonts.battambang(),
+                                      )),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    MaterialButton(
+                                      onPressed: fillAdministration,
+                                      child: const Text('Fill Administration'),
+                                    ),
+                                    MaterialButton(
+                                      onPressed: fillUser,
+                                      child: const Text('Fill User'),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                    MaterialButton(
-                      onPressed: fillUser,
-                      child: const Text('Fill User'),
+                    const VerticalDivider(),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: SvgPicture.asset(
+                              'assets/images/logo.svg',
+                              width: 250,
+                            ),
+                          ),
+                          SvgPicture.asset(
+                            'assets/images/login-image.svg',
+                            width: 400,
+                          ),
+                        ],
+                      ),
                     )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
