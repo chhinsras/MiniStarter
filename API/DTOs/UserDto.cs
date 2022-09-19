c76fadf691298c12621f33418de6eda74435d830
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2022-09-19 11:58:36 +0700

    typo

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -13,7 +13,7 @@ namespace API.DTOs
         public string? Email { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public String? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? ImageUrl { get; set; }
         public bool IsActive { get; set; }
         public string? Token { get; set; }
