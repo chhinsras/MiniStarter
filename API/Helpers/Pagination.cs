6066858b361dee6ccbf12a0d906ccbe41e392f9a
Chhin Sras <31751665+chhinsras@users.noreply.github.com>
2022-10-06 09:44:47 +0700

    Pagination

--- API/Helpers/Pagination.cs
+++ API/Helpers/Pagination.cs
@@ -32,3 +32,14 @@ public class PagedList<T> : List<T>
         return new PagedList<T>(items, count, pageNumber, pageSize);
     }
 }
+public class PaginationParams
+{
+    private const int MaxPageSize = 50;
+    public int PageNumber { get; set; } = 1;
+    private int _pageSize = 6;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
+    }
+}
